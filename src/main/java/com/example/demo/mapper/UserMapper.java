f45fec83250b6143925ac8bc87397efa868769ba
yangle <yangle@kyee.com.cn>
2018-12-30 16:00:10 +0800

    annotation project

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -28,7 +28,7 @@ public interface UserMapper {
 	})
 	UserEntity getOne(Long id);
 
-	@Insert("INSERT INTO users(userName,passWord,user_sex) VALUES(#{userName}, #{passWord}, #{userSex})")
+	@Insert("INSERT INTO users(userName,passWord,user_sex,nick_name) VALUES(#{userName}, #{passWord}, #{userSex}, #{nickName})")
 	void insert(UserEntity user);
 
 	@Update("UPDATE users SET userName=#{userName},nick_name=#{nickName} WHERE id =#{id}")
--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,26 @@ public class UserController {
 		return all;
 		
 	}
+	
+	@RequestMapping("/getUser")
+	public UserEntity getUser(Long id) {
+		UserEntity one = userMapper.getOne(id);
+		return one;
+	}
+	
+	@RequestMapping("/addUser")
+	public void addUser(UserEntity user) {
+		userMapper.insert(user);
+	}
+	
+	@RequestMapping("/updateUser")
+	public void updateUser(UserEntity user) {
+		userMapper.update(user);
+	}
+	
+	@RequestMapping(value="/delete/{id}")
+	public void deleteUser(@PathVariable("id")Long id) {
+		userMapper.delete(id);
+	}
+	
 }
